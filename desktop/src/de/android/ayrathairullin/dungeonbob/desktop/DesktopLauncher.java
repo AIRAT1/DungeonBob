35c26d5a145c54e74b7a4b0752245dfd6c8f7775
Ayrat <ayrat1@mail.ru>
2018-03-31 20:41:50 +0300

    Initial commit

---
+++ desktop/src/de/android/ayrathairullin/dungeonbob/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.android.ayrathairullin.dungeonbob.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.android.ayrathairullin.dungeonbob.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
