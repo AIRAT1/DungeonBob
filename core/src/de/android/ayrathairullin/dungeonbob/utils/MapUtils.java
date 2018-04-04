08685b96e50c432a60d562c79f66908034d7e550
Ayrat <ayrat1@mail.ru>
2018-04-04 22:17:09 +0300

    254

--- core/src/de/android/ayrathairullin/dungeonbob/utils/MapUtils.java
+++ core/src/de/android/ayrathairullin/dungeonbob/utils/MapUtils.java
@@ -1,6 +1,7 @@
 package de.android.ayrathairullin.dungeonbob.utils;
 
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
@@ -21,14 +22,15 @@ import de.android.ayrathairullin.dungeonbob.gameobjects.Zombie;
 
 public class MapUtils {
     public static TiledMap map;
-    public static void initialize(TiledMap map){
-        MapUtils.map= map;
+
+    public static void initialize(TiledMap map) {
+        MapUtils.map = map;
     }
 
     // create a pool of rectangle objects for collision detection
     private static Pool<Rectangle> rectPool = new Pool<Rectangle>() {
         @Override
-        protected Rectangle newObject () {
+        protected Rectangle newObject() {
             return new Rectangle();
         }
     };
@@ -36,9 +38,9 @@ public class MapUtils {
     private static Array<Rectangle> tiles = new Array<Rectangle>();
 
 
-    public static Array<Rectangle> getTiles (int startX, int startY, int endX, int endY, String layerName) {
+    public static Array<Rectangle> getTiles(int startX, int startY, int endX, int endY, String layerName) {
 
-        TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(layerName);
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(layerName);
         // return the rectangle objects to the pool from previous frame
         rectPool.freeAll(tiles);
 
@@ -60,60 +62,70 @@ public class MapUtils {
         return tiles;
     }
 
-    /** this method returns the tiles which are near to a sprite horizontally */
-    public static Array<Rectangle> getHorizNeighbourTiles(Vector2 velocity,Sprite sprite,String layerName){
+    /**
+     * this method returns the tiles which are near to a sprite horizontally
+     */
+    public static Array<Rectangle> getHorizNeighbourTiles(Vector2 velocity, Sprite sprite, String layerName) {
         int startX, startY, endX, endY;
         //if the sprite is moving right, get the tiles to its right side
         if (velocity.x > 0) {
-            startX = endX = (int)(sprite.getX() + sprite.getWidth()+velocity.x);
+            startX = endX = (int) (sprite.getX() + sprite.getWidth() + velocity.x);
         }
         //if the sprite is moving left, get the tiles to its left side
         else {
-            startX = endX = (int)(sprite.getX() + velocity.x);
+            startX = endX = (int) (sprite.getX() + velocity.x);
         }
-        startY = (int)(sprite.getY());
-        endY = (int)(sprite.getY() + sprite.getHeight());
+        startY = (int) (sprite.getY());
+        endY = (int) (sprite.getY() + sprite.getHeight());
 
         // get the tiles
-        return getTiles(startX, startY, endX, endY,layerName);
+        return getTiles(startX, startY, endX, endY, layerName);
     }
 
-    /** this method returns the tiles which are near to a sprite vertically */
-    public static  Array<Rectangle> getVertNeighbourTiles(Vector2 velocity, Sprite sprite, String layerName){
+    /**
+     * this method returns the tiles which are near to a sprite vertically
+     */
+    public static Array<Rectangle> getVertNeighbourTiles(Vector2 velocity, Sprite sprite, String layerName) {
         int startX, startY, endX, endY;
         //if sprite is moving up, get the tiles above it
         if (velocity.y > 0) {
-            startY = endY = (int)(sprite.getY() + sprite.getHeight() );
+            startY = endY = (int) (sprite.getY() + sprite.getHeight());
 
         }
         // if sprite is moving down, get the tiles below it
         else {
-            startY = endY = (int)(sprite.getY() + velocity.y);
+            startY = endY = (int) (sprite.getY() + velocity.y);
         }
-        startX = (int)(sprite.getX());
-        endX = (int)(sprite.getX() + sprite.getWidth());
+        startX = (int) (sprite.getX());
+        endX = (int) (sprite.getX() + sprite.getWidth());
         // get the tiles
-        return getTiles(startX, startY, endX, endY,layerName);
+        return getTiles(startX, startY, endX, endY, layerName);
     }
 
-    public static void spawnEnemies(Array<Enemy> enemies, float width, float height, TextureAtlas texturePack){
+    public static void spawnEnemies(Array<Enemy> enemies, float width, float height, TextureAtlas texturePack) {
 
         Iterator<MapObject> mapObjectIterator = map.getLayers().get("Enemies").getObjects().iterator();
 
-        float unitScale= GameConstants.UNIT_SCALE;
-        while(mapObjectIterator.hasNext()){
+        float unitScale = GameConstants.UNIT_SCALE;
+        while (mapObjectIterator.hasNext()) {
             // get the map object from iterator
             MapObject mapObject = mapObjectIterator.next();
 
             // if the name of the object is "zombie" as we have given
-            if(mapObject.getName().equals("zombie")){
+            if (mapObject.getName().equals("zombie")) {
                 if (mapObject instanceof RectangleMapObject) {
                     Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
                     //create a zombie object and place it in that location
                     Zombie zombie = new Zombie(width, height, texturePack.findRegion(GameConstants.ZOMBIE_SPRITE_SHEET),
                             rectangle.x * unitScale, rectangle.y * unitScale);
                     enemies.add(zombie);
+                }else {
+                    Gdx.app.log("LOG", "No Rectangle"); // TODO ERROR HERE - NO RECTANGLE OBJECTS DETECTED
                 }
+
+//                Zombie zombie = new Zombie(width, height, texturePack.findRegion(GameConstants.ZOMBIE_SPRITE_SHEET),
+//                       330 * unitScale, 50 * unitScale);
+//                enemies.add(zombie);
             }
         }
     }
